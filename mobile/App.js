ae208d4ec92a90a9032ac6d90f46c6c2cfdb952b
Eno <duartebrian@hotmail.com>
2020-08-22 20:36:42 -0300

    created the listings screen

--- mobile/App.js
+++ mobile/App.js
@@ -2,16 +2,17 @@ import React from "react";
 import Welcome from "./app/screens/welcome";
 import ViewImage from "./app/screens/viewImage";
 import Details from "./app/screens/details";
-import { StyleSheet, StatusBar, SafeAreaView } from "react-native";
+import { StyleSheet, StatusBar, SafeAreaView, View } from "react-native";
 import Messages from "./app/screens/messages";
 import Account from "./app/screens/account";
+import Listings from "./app/screens/listings";
 
 console.log(StatusBar.currentHeight);
 
 export default function App() {
   return (
     <SafeAreaView style={styles.screen}>
-      <Account />
+      <Listings />
     </SafeAreaView>
   );
 }
---
+++ mobile/app/screens/listings.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { View, StyleSheet, FlatList } from "react-native";
+
+import Card from "../components/card";
+
+const listings = [
+  {
+    id: 1,
+    title: "Red jacket for sale",
+    price: 99,
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Couch in good codition",
+    price: 815,
+    image: require("../assets/couch.jpg"),
+  },
+];
+
+function Listings() {
+  return (
+    <View style={{ padding: 20 }}>
+      <FlatList
+        data={listings}
+        keyExtractor={(i) => i.id + ""}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subTitle={"$ " + item.price}
+            image={item.image}
+          />
+        )}
+      />
+    </View>
+  );
+}
+
+export default Listings;
